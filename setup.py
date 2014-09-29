05fdaef90a1d7721130fbcfc3ad123e811853a62
Vincent <vxgmichel@gmail.com>
2014-09-29 23:14:36 +0200

    typo fixed

--- setup.py
+++ setup.py
@@ -14,11 +14,14 @@ class UploadGhPages(Command):
         pass
 
     def run(self):
-        # Import
-        from fabric.api import local
-        # Delete gh-pages branch
-        try: local('git branch -D gh-pages ')
-        except: pass
+        # Import fabric
+        try: 
+            from fabric.api import local
+        # Import subprocess
+        except ImportError:
+            from subprocess import call
+            from functools import partial
+            local = partial(call, shell=True)
         # Create gh-pages branch
         local('git checkout --orphan gh-pages ')
         # Unstage all
@@ -28,6 +31,8 @@ class UploadGhPages(Command):
         # No jekyll file
         local('touch .nojekyll')
         local('git add .nojekyll')
+        # Add Readme
+        local('git add README.md')
         # Add html content
         local('git add docs/build/html/* -f ')
         # Move html content
@@ -38,11 +43,14 @@ class UploadGhPages(Command):
         local('git push --set-upstream origin gh-pages -f ')
         # Back to master
         local('git checkout master -f ')
+        # Delete branch
+        local('git branch -D gh-pages ')
 
 
-setup(name = 'pygame-mctools',
+
+setup(name = 'pygame-mvctools',
       version = '0.1.0',
-      description = 'High-level set of Python modules designed for writing games',
+      description = 'High-level set of modules designed for writing games.',
       packages = find_packages(),
       cmdclass = {'upload_gh_pages': UploadGhPages}
      )
